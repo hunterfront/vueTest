240094bed3a7de0f9201bc793ff07d102c47295e
wuhantao <wuhantao1216@gmail.com>
2021-11-08 22:59:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import Vue from "./vue.esm.browser.js";
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("beforeMounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("destroyed");
+  },
+});
+
+console.log("fdf");
